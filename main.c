f8a6f9f87050cc2e1b87aaa08aa758df7f426f68
shaun-wong1 <147071940+shaun-wong1@users.noreply.github.com>
2025-05-10 11:50:50 +0800

    memory freed and full dictionary is prinatble in quick order
    - fast
    - better
    - working

--- main.c
+++ main.c
@@ -11,8 +11,15 @@ int main() {
     FILE *dictionary_FILE;
     dictionary_FILE = fopen("dictionary.txt", "r");
 
-    Node head_node;
+    Node* head_node = malloc(sizeof(Node));
+    Node* end_node = NULL;
     char line_buff[16]; //stores the first 16 chars of the line
+
+    fgets(line_buff, 16, dictionary_FILE);
+    string line = constructor(16);
+    add_characters(&line, line_buff);
+    end_node = newNode(line);
+    head_node->next = end_node;
 
 
 
@@ -20,18 +27,37 @@ int main() {
         
         string line = constructor(16);
         add_characters(&line, line_buff);
-        append(&head_node, line);
-
-        char* lineSTRING; //creates new version of line that is a char* and not custom string class
-        for (int x = 1; x < line.length + 1; x+= 1000) {
-            lineSTRING = line.body;
-        }
-        printf(" %s \n", lineSTRING);
+        end_node->next = newNode(line);
+        end_node = end_node->next;
+        
+        //printf(" %s \n", line.body);
 
     } //read the content of the first line and store it in line_buff
 
     //load th first line into the string struc and make sure it has valid capacity and length
 
     fclose(dictionary_FILE);
+
+
+    end_node = head_node;
+    while (end_node->next != NULL) { //loops through, first creating a new (temp) node to contain the next node
+        Node* temp_node = end_node->next;
+        destructor(&end_node->word); //next frees the word/contents of the node
+        free(end_node); //frees the node itself
+        end_node = temp_node;//loops back
+    }
+
+    destructor(&end_node->word);// catches the case where the final node exsits but not being processed
+    free(end_node);// above since while loop checks for the next node
+    //so this is a catch for the very last node
+
+    
+
+    //print out the dictionary
+    //free dictionary memory
+    //1. free the words at each time
+    //2. move the next of the node to some other place
+    //3. free the memory needed for the newNodes
+    //4. free the next of the node
 
 }
